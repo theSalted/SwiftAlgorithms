8576b59d83ff34a561bd7b0f6a8b58d83dc6f94b
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-03-17 16:57:10 -0500

    understood dfs and num is island

--- NumIsIslands.swift
+++ NumIsIslands.swift
@@ -2,29 +2,31 @@
 class Solution {
     func numIslands(_ grid: [[Character]]) -> Int {
         var grid = grid
-        var numberOfIslands = 0
+        var result = 0
+
+        func dfs(_ row: Int, _ col: Int) {
+            if row < 0 || col < 0 || row >= grid.count || col >= grid[0].count {
+                return
+            }
+
+            if grid[row][col] != "1" { return }
+            grid[row][col] = "0"
+
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+        }
 
         for row in 0..<grid.count {
             for col in 0..<grid[0].count {
                 if grid[row][col] == "1" {
-                    dfs(row, col, &grid)
-                    numberOfIslands += 1
+                    dfs(row, col)
+                    result += 1
                 }
-            }
+             }
         }
 
-        return numberOfIslands
-    }
-
-    func dfs(_ row: Int, _ col: Int, _ grid: inout [[Character]]) {
-        if row < 0 || row >= grid.count || col < 0 || col >= grid[0].count { return }
-        if grid[row][col] != "1" { return }
-        grid[row][col] = "0"
-
-        dfs(row - 1, col, &grid)
-        dfs(row + 1, col, &grid)
-        dfs(row, col - 1, &grid)
-        dfs(row, col + 1, &grid)
+        return result        
     }
 }
-
