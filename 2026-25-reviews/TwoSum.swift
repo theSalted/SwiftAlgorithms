ff4fec4a345fa343f3dfb8c9d4560b876f0435ae
Yuhao Chen <hello@chenyuhao.me>
2026-05-05 01:53:44 -0700

    Create TwoSum.swift

---
+++ 2026-25-reviews/TwoSum.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var store = [Int: Int]()
+
+        for (index, num) in nums.enumerated() {
+            let diff = target - num
+
+            if let prev = store[diff] {
+                return [prev, index]
+            }
+
+            store[num] = index
+        }
+
+        return []
+    }
+}
